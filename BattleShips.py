d0eeead8376137b0e935230220ac71878b87684b
J-Rooke <joshrooke10@gmail.com>
2021-11-01 18:01:48 +0000

    added function to randomly generate ships

--- BattleShips.py
+++ BattleShips.py
@@ -1,3 +1,5 @@
+from random import randint
+
 #welcome message to the player
 print("""###############################\n
 WELCOME TO CMD LINE BATTLESHIPS\n
@@ -5,36 +7,59 @@ WELCOME TO CMD LINE BATTLESHIPS\n
 
 print(input("PRESS ENTER TO CONTINUE"))
 
-#info for how to set ships
-print("")
-
-#initializing battleships_arena to all water (10 x 10 grid and "W" represents water)
-battleships_arena = {}
-
-for i in range(1, 11):
-    battleships_arena[i] = {}
-    for y in range(1, 11):
-        battleships_arena[i][y] = "W"
-
-#function to set the squares in the arena which will be your ships ("S represnts ship")
-def set_ships(id, row, col):
-    #add logic to ensure ships are not set randomly and correct length 
-    battleships_arena[row][col] = "S"
-
 #function to print the arena to the cmd 
-def print_arena():
+def print_arena(dict):
     print("   ", end=" ")
     for i in range(1, 11):
         print("{}".format(i), end ="   ")  
-    for row in battleships_arena.keys():
+    for row in dict.keys():
         print("\n")
         if row < 10:
             print("{}".format(row), end ="   ")
         else:
             print("{}".format(row), end ="  ")       
-        for col, value in battleships_arena[row].items():
+        for col, value in dict[row].items():
             if value == "W":
-                print("-", end="   ")     
+                print("-", end="   ")   
+            elif value == "S" or value == "C" or value == "D" or value == "B":
+                print("X", end="   ")
+            elif value == "M":
+                print("M", end="   ")    
 
 
-print_arena()
+#initializing battleships_arena and hidden arena to all water (10 x 10 grid and "W" represents water)
+battleships_arena = {}
+battleships_arena_hidden = {}
+
+for i in range(1, 11):
+    battleships_arena[i] = {}
+    battleships_arena_hidden[i] = {}
+    for y in range(1, 11):
+        battleships_arena[i][y] = "W" 
+        battleships_arena_hidden[i][y] = "W" 
+
+
+
+#function to generate random ships
+def generate_ships(ship, num=1):
+    count = num
+    while count > 0:
+        row = randint(1, 10)
+        column = randint(1, 10)
+        if battleships_arena_hidden[row][column] == "W":
+            battleships_arena_hidden[row][column] = ship
+            count -= 1
+
+generate_ships("S", 3) 
+
+print_arena(battleships_arena)
+print("   ")
+print_arena(battleships_arena_hidden)
+
+
+
+    
+
+
+    
+
